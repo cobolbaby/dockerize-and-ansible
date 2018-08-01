e30b603cbc59e16e0671486f9bc6cedacbf36c70
liguofang <liguofang@liguofangdeMacBook-Pro.local>
2018-08-01 14:22:04 +0800

    feat: 修正harbor启动脚本

--- harbor/lanuch.sh
+++ harbor/lanuch.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-wget https://github.com/vmware/harbor/archive/v1.5.2.tar.gz
-tar -zxvf v1.5.2.tar.gz -C ／opt
-cd /opt/harbor
+wget https://storage.googleapis.com/harbor-releases/harbor-offline-installer-v1.5.2.tgz
+tar -zxvf harbor-offline-installer-v1.5.2.tgz -C /opt
 
 # harbor.cfg 修改hostname、harbor_admin_password
-cp config/harbor.cfg .
-./install.sh
+cp config/harbor.cfg /opt/harbor/
+
+cd /opt/harbor && ./install.sh
