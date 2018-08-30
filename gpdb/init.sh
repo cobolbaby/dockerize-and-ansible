d8d9909bb7bba518da4dfccb0b62cf987a4265b8
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-08-30 10:23:41 +0800

    feat: 优化Dockerfile

--- gpdb/init.sh
+++ gpdb/init.sh
@@ -8,3 +8,4 @@ else
 fi
 
 TAGNAME=development/gpdb:${BRANCH}
+PROXY=http://10.190.40.39:18118/
--- node/build.sh
+++ node/build.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-NAME=cobol/nodejs:8.11
+NAME=development/node:8.11
+PROXY=http://10.190.40.39:18118/
 
 # 添加TS编译
 # gulp build
 
-docker build --rm -f Dockerfile -t $NAME .
+docker build --rm -f Dockerfile -t $NAME --build-arg http_proxy=${PROXY} --build-arg https_proxy=${PROXY} .
