a587d46b420f471dde07f13eae7e71665b8c9f81
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-09-04 18:46:56 +0800

    feat: 自动获取spark master的ID

--- spark/deploy/submit.sh
+++ spark/deploy/submit.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Getting container ID of the Spark master..."
-CONTAINERID="eba80a9027bf"
+CONTAINERID=`docker ps --filter name=sparkm --format "{{.ID}}"`
 
-
-
+echo "Copying count.py script to the Spark master..."
 docker cp ./tasks ${CONTAINERID}:/opt/
 
+echo "Running Spark job..."
+docker exec -ti ${CONTAINERID} spark-submit /opt/tasks/python/pi.py
 # docker exec -ti ${CONTAINERID} spark-submit --master spark://sparkmaster:7077 /opt/tasks/pi.py
-docker exec -ti ${CONTAINERID} spark-submit /opt/tasks/pi.py
