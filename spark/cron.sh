79b94c9ada17a83aa1e771d7ddaf6fe2e8835063
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-09-05 17:39:10 +0800

    fix: ansible定时任务的设置

--- spark/cron.sh
+++ spark/cron.sh
@@ -3,4 +3,4 @@
 
 # 传出配置文件
 ansible -i $INVENTORY_FILE master -m cron -a 'name="pi" state=absent'
-ansible -i $INVENTORY_FILE master -m cron -a 'name="pi" hour=*/2 job="/opt/sparkv2/submit.sh >> /opt/sparkv2/logs/jobs.log 2>&1"'
+ansible -i $INVENTORY_FILE master -m cron -a 'name="pi" minute=0 hour=*/2 job="/opt/sparkv2/submit.sh >> /opt/sparkv2/logs/jobs.log 2>&1"'
