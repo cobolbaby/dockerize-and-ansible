2e55022969b80447c45c90ff4a0647292d43fc5c
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-09-05 10:12:36 +0800

    feat: 支持在任意目录执行submit脚本

--- spark/deploy/submit.sh
+++ spark/deploy/submit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+CURR_DIR=`dirname $0`
+
 echo "Getting container ID of the Spark master..."
 CONTAINERID=`docker ps --filter name=sparkm --format "{{.ID}}"`
 
 echo "Copying count.py script to the Spark master..."
-docker cp ./tasks ${CONTAINERID}:/opt/
+docker cp ${CURR_DIR}/tasks ${CONTAINERID}:/opt/
 
 echo "Running Spark job..."
 docker exec -ti ${CONTAINERID} spark-submit --master spark://sparkmaster:7077 /opt/tasks/python/pi.py
