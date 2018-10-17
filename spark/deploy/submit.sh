3c673b2d85b17a1add3a9cf41a842ea90e79a858
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-10-17 14:37:08 +0800

    feat: 启用spark speculation

--- spark/deploy/submit.sh
+++ spark/deploy/submit.sh
@@ -8,9 +8,5 @@ CURR_DIR=`dirname $0`
 echo "Getting container ID of the Spark master..."
 CONTAINERID=`docker ps --filter name=sparkm --format "{{.ID}}"`
 
-# TODO::REMOVE
-# echo "Copying count.py script to the Spark master..."
-# docker cp ${CURR_DIR}/tasks ${CONTAINERID}:/opt/
-
 echo "Running Spark job..."
 docker exec ${CONTAINERID} /opt/spark/bin/spark-submit --master spark://sparkmaster:7077 --total-executor-cores 2 --executor-cores 2 /opt/tasks/python/pi.py
