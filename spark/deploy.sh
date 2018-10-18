a640e58a250287813941a001ded60cd884193dc1
Cobolbaby <Zhang.Xing-Long@inventec.com>
2018-10-18 10:48:44 +0800

    fix: Uncaught exception while reverting partial writes to /tmp/spark

--- spark/deploy.sh
+++ spark/deploy.sh
@@ -10,7 +10,7 @@ fi
 
 # 传输文件
 ansible -i $INVENTORY_FILE master -m copy -a "src=deploy/ dest=/opt/sparkv2"
-ansible -i $INVENTORY_FILE master -m file -a "dest=/disk6/spark/spark-events mode=777 state=directory" -b
+ansible -i $INVENTORY_FILE all -m file -a "dest=/disk6/spark mode=777 state=directory" -b
 
 # 执行启动命令
 ansible -i $INVENTORY_FILE all -m raw -a "docker pull ${REGISTRY}/${TAGNAME}" -f 5 -b
