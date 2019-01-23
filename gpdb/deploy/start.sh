3a8e14fc1ac4ff079d436e780bb8218387e32db4
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-01-23 18:02:24 +0800

    feat: keepalived dockerfile

--- gpdb/deploy/start.sh
+++ gpdb/deploy/start.sh
@@ -5,10 +5,6 @@ echo "启动参数: $*"
 export REGISTRY="$1"
 export TAGNAME="$2"
 
-docker stack rm gpdb_master
-docker stack rm gpdb_segment
-sleep 10  # 等10秒后执行下一条
-
 docker stack deploy -c docker-compose-segment.yml gpdb_segment
 sleep 30  # 等30秒后执行下一条
 docker stack deploy -c docker-compose-master.yml gpdb_master
