9e5a52e7550855e700715bcf706714bf00be7a0e
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-02-10 22:54:50 +0800

    style: 移除etcd测试部署脚本

--- etcd/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. ./init.sh
-. ./clean.sh
-
-cd build
-docker build --rm -f Dockerfile -t ${REGISTRY}/${TAGNAME} --build-arg http_proxy=${PROXY} --build-arg https_proxy=${PROXY} .
-docker push ${REGISTRY}/$TAGNAME
