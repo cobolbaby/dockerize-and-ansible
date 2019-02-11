e36d622aa3347c6cea59ad6519154037607a1232
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-02-11 13:08:59 +0800

    feat: 调整spark容器编排

--- spark/deploy/start.sh
+++ spark/deploy/start.sh
@@ -6,10 +6,9 @@ cd /opt/sparkv2
 echo "启动参数: $*"
 export REGISTRY="$1"
 export TAGNAME="$2"
-export EXTERNAL_IP=`hostname -I | cut -d " " -f 1`
+# export EXTERNAL_IP=`hostname -I | cut -d " " -f 1`
 
-docker stack rm spark_worker
-docker stack rm spark_master
+docker stack rm spark_master spark_worker
 sleep 10  # 等10秒后执行下一条
 
 docker stack deploy -c docker-compose-master.yml spark_master
