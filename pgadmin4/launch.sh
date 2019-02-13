b6f3d035d4a4dfaa8e0a7b24b7eae3ab1cb0fd47
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-02-13 10:36:02 +0800

    fix: 指定pgadmin版本以适配greenplum

--- pgadmin4/launch.sh
+++ pgadmin4/launch.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-docker pull dpage/pgadmin4
+
+# 采用最新版镜像4.2，在webui页面创建Greenplum数据库连接时，会提示异常:
+# permission denied: "pg_settings" is a system catalog
+# GPDB SQL执行语句: 
+# UPDATE pg_settings SET setting = 'escape' WHERE name = 'bytea_output';
+# 因此回退至4.1版本
+docker pull dpage/pgadmin4:4.1
 
 docker run -p 80:80 \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=123456" \
-    -d dpage/pgadmin4
+    -d dpage/pgadmin4:4.1
