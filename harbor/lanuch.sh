9a72e2b71f8b44190c8c219c6bec5037f5564c54
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-02-22 10:52:22 +0800

    feat: 升级Harbor版本至1.7.1，修复安装时证书引发的问题

--- harbor/lanuch.sh
+++ harbor/lanuch.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-wget https://storage.googleapis.com/harbor-releases/harbor-offline-installer-v1.5.2.tgz
-tar -zxvf harbor-offline-installer-v1.5.2.tgz -C /opt
+wget https://storage.googleapis.com/harbor-releases/release-1.7.0/harbor-offline-installer-v1.7.1.tgz
+tar -zxvf harbor-offline-installer-v1.7.1.tgz -C /opt
 
 # harbor.cfg 修改hostname、harbor_admin_password
 cp config/harbor.cfg /opt/harbor/
