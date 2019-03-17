776b567fda273ffc25b7a5386b8c5405461d5707
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-03-17 12:26:33 +0800

    feat: 新增bcc工具

---
+++ bcc/bcc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -it --rm \
+  --privileged \
+  -v /lib/modules:/lib/modules:ro \
+  -v /usr/src:/usr/src:ro \
+  -v /etc/localtime:/etc/localtime:ro \
+  --workdir /usr/share/bcc/tools \
+  zlim/bcc
