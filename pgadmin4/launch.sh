2add60182d311e88bce115ee265837e67f3fa7b1
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-04-03 11:05:27 +0800

    feat: pgadmin4升级至4.3

--- pgadmin4/launch.sh
+++ pgadmin4/launch.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-# 采用最新版镜像4.2，在webui页面创建Greenplum数据库连接时，会提示异常:
+# 采用镜像4.2，在webui页面创建Greenplum数据库连接时，会提示异常:
 # permission denied: "pg_settings" is a system catalog
 # GPDB SQL执行语句: 
 # UPDATE pg_settings SET setting = 'escape' WHERE name = 'bytea_output';
-# 因此回退至4.1版本
-docker pull dpage/pgadmin4:4.1
 
 docker run -p 80:80 \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=123456" \
-    -d dpage/pgadmin4:4.1
+    -d dpage/pgadmin4:4.3
