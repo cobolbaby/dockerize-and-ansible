2084789ee9a924dcc4f34e15854e5d7b477a1747
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-07-08 18:37:56 +0800

    feat: 完善Kafka的日志记录配置以及Zookeeper持久化配置

--- kafka/deploy.dev/bin/start.sh
+++ kafka/deploy.dev/bin/start.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 BROKER_CLUSTER_IP=(10.191.5.218 10.191.5.233 10.191.4.54)
 BROKER_CLUSTER_VERSION=5.0.3
-ZOOKEEPER_CLUSTER_IP=(10.191.5.218 10.191.5.233 10.191.4.54)
+ZOOKEEPER_CLUSTER=zoo1:2181
 HARBOR_REGISTRY=harbor.inventec.com
 
 # Ref: https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
@@ -16,15 +16,17 @@ done
 
 export BROKER_VERSION=${BROKER_CLUSTER_VERSION}
 export REGISTRY=${HARBOR_REGISTRY}
+export ZOOKEEPER=${ZOOKEEPER_CLUSTER}
 
 echo "Harbor仓库地址: ${REGISTRY}"
-echo "KAFKA集群IP为: $BROKER_NODE1_IP, $BROKER_NODE2_IP, $BROKER_NODE3_IP"
-echo "KAFKA版本为: $BROKER_VERSION"
+echo "KAFKA集群IP为: ${BROKER_NODE1_IP}, ${BROKER_NODE2_IP}, ${BROKER_NODE3_IP}"
+echo "KAFKA版本为: ${BROKER_VERSION}"
+echo "ZOOKEEPER版本为: ${ZOOKEEPER}"
 
 # Ref: https://stackoverflow.com/questions/45804955/zookeeper-refuses-kafka-connection-from-an-old-client
 # 确保容器重新创建
-# docker stack rm kafka
-# docker stack rm zookeeper
+docker stack rm kafka
+docker stack rm zookeeper
 
 docker stack deploy -c docker-compose-kafka.yml kafka
-# docker stack deploy -c docker-compose-zookeeper.yml zookeeper
+docker stack deploy -c docker-compose-zookeeper.yml zookeeper
