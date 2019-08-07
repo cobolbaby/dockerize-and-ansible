3617a6c55a9b64f7cde1744a24bb4542d7af58b2
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-08-07 10:53:34 +0800

    fix: Mongo容器挂载路径为dbpath的父目录时出现的数据丢失问题

--- mongo/deploy.dev.sh
+++ mongo/deploy.dev.sh
@@ -5,8 +5,10 @@ INVENTORY_FILE=../inventory.dev
 
 # 传出配置文件
 ansible -i $INVENTORY_FILE mongo -m copy -a "src=deploy.dev/ dest=/opt/mongo"
-# ansible -i $INVENTORY_FILE mongo -m file -a "path=/data/ssd1/mongo state=absent" -f 5
-ansible -i $INVENTORY_FILE mongo -m file -a "dest=/data/ssd1/mongo owner=999 group=999 mode=700 state=directory" -f 5 -b 
+# ansible -i $INVENTORY_FILE mongo -m file -a "path=/data/ssd1/mongo state=absent"
+ansible -i $INVENTORY_FILE mongo -m file -a "dest=/data/ssd1/mongo/configdb owner=999 group=999 mode=700 state=directory" -b
+ansible -i $INVENTORY_FILE mongo -m file -a "dest=/data/ssd1/mongo/db owner=999 group=999 mode=700 state=directory" -b
+ansible -i $INVENTORY_FILE mongo -m file -a "dest=/data/ssd1/mongo/logs owner=999 group=999 mode=700 state=directory" -b
 
 # 执行启动命令
 ansible -i $INVENTORY_FILE mongo -m shell -a "cd /opt/mongo && docker-compose up -d"
