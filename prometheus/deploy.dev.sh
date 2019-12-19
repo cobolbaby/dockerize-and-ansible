021588bd63d8f9d4a3f5d595300183b52936aff2
Cobolbaby <Zhang.Xing-Long@inventec.com>
2019-12-19 16:18:28 +0800

    feat: 完善Telegraf配置文件精简方法

--- prometheus/deploy.dev.sh
+++ prometheus/deploy.dev.sh
@@ -6,6 +6,9 @@
 # 推送启动脚本至部署目录
 INVENTORY_FILE=../inventory.dev
 
+# docker run --rm telegraf:1.13.0-alpine telegraf config > telegraf.default.conf
+# sed '/^\s*#\|^$/d' ./deploy.dev/config/telegraf/telegraf.default.conf > ./deploy.dev/config/telegraf/telegraf.conf
+
 # 传出配置文件
 ansible -i $INVENTORY_FILE monitor1 -m copy -a "src=deploy.dev/ dest=/opt/prometheus" -b
 
