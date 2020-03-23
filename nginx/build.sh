397bfe3f4a9a146bb2612ae72c2c550cee55bd23
Cobolbaby <Zhang.Xing-Long@inventec.com>
2020-03-23 14:03:44 +0800

    feat: 清理不再推荐使用的脚本

--- nginx/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. ./init.sh
-
-docker build --rm -f Dockerfile -t $TAGNAME .
-docker tag $TAGNAME ${REGISTRY}/$TAGNAME
-docker push ${REGISTRY}/$TAGNAME
--- nginx/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ./init.sh
-
-sh clean.sh
-docker pull ${REGISTRY}/$TAGNAME
-date_str=`date "+%Y%m%d%H%M"`
-docker run -d --name nginx_${date_str} --net=host -h nginx -p 80:80 -v /opt/shujuguan/www/feapp:/opt/shujuguan/www/feapp ${REGISTRY}/$TAGNAME
-docker logs -f nginx_${date_str}
