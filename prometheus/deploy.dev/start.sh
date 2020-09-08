691c36c73c22357bb0e1d6a7ab80c981c081f88e
Cobolbaby <Zhang.Xing-Long@inventec.com>
2020-09-08 22:36:58 +0800

    feat: 剔除部分服务的监控，已有替代方案

--- prometheus/deploy.dev/start.sh
+++ prometheus/deploy.dev/start.sh
@@ -2,7 +2,6 @@
 set -e
 cd `dirname $0`
 
-export BROKER1_ADDR=${IP1}:9092 BROKER2_ADDR=${IP2}:9092 BROKER3_ADDR=${IP3}:9092
 export PG_URI=postgresql://${USERNAME1}:${PASSWORD1}@${IP4}:5493/postgres?sslmode=disable
 export ES_URI=http://${IP5}:9200
 export MYSQL_URI="${USERNAME2}:${PASSWORD2}@(${IP6}:3306)/"
