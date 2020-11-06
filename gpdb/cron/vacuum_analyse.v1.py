9a09f60f85c59709bae8c3acfeaac8c692f15cbe
Cobolbaby <Zhang.Xing-Long@inventec.com>
2020-11-06 11:13:47 +0800

    feat: 为vacuum添加可维护窗口期配置，以及优化gp的vacuum操作对象

--- gpdb/cron/vacuum_analyse.v1.py
+++ gpdb/cron/vacuum_analyse.v1.py
@@ -56,7 +56,7 @@ class Vaccum(object):
                 n_dead_pages,
                 n_live_pages,
                 dead_tup_ratio,
-                CASE WHEN dead_tup_ratio >= 25 THEN 'VACUUM_FULL' WHEN dead_tup_ratio >= 5 THEN 'VACUUM' ELSE '' END as strategy
+                CASE WHEN dead_tup_ratio >= 25 THEN 'VACUUM_FULL' WHEN dead_tup_ratio >= 10 THEN 'VACUUM' ELSE '' END as strategy
             from
                 (
                     select
@@ -74,13 +74,25 @@ class Vaccum(object):
                         join pg_namespace pgn on pgc.relnamespace = pgn.oid
                     where
                         btdrelpages > btdexppages
-                        and btdrelpages - btdexppages > 10000
-                        and pgn.nspname <> 'bsi_old'
-                        and pgc.relname <> 'pinresult'
-                        and pgc.relname <> 'gp_persistent_relation_node'
+                        and (
+                            (
+                                btdrelpages - btdexppages > 10000
+                                and pgn.nspname <> 'pg_catalog'
+                                and pgn.nspname <> 'bsi_old'
+                                and pgc.relname !~ 'pinresult'
+                            )
+                            or 
+                            (
+                                pgn.nspname = 'pg_catalog'
+                                and pgc.relname <> 'gp_persistent_relation_node' 
+                                -- and btdrelpages - btdexppages > 100
+                            )
+                        )
+                    order by
+                        dead_tup_ratio desc
                 ) bloat
-            order by
-                dead_tup_ratio desc
+            limit
+                30
         """
 
         cur = self.conn.cursor()
@@ -117,11 +129,9 @@ class Vaccum(object):
                         and round(n_dead_tup * 100 / (n_live_tup + n_dead_tup), 2) > 0
                     order by
                         dead_tup_ratio desc
-                    limit
-                        100
                 ) bloat
-            order by
-                dead_tup_ratio desc
+            limit
+                10
         """
 
         cur = self.conn.cursor()
@@ -146,7 +156,7 @@ class Vaccum(object):
             cur = self.conn.cursor()
             for cmd in cmds:
                 verbose = "{0} {1} -- [{2}]".format(
-                    cmd, '.'.join([schema, table]), datetime.utcnow())
+                    cmd, '.'.join([schema, table]), datetime.now())
                 print(verbose)
                 cur.execute(verbose)
 
@@ -155,12 +165,24 @@ class Vaccum(object):
             self.conn.set_isolation_level(isolation_level)
 
 
-def main(dsns=[]):
+def main(dsns=[], maintain_window={}):
+
+    maintain_start_time = maintain_window.get(
+        'start_time') if maintain_window.get('start_time') is not None else '00:00'
+    maintain_end_time = maintain_window.get(
+        'end_time') if maintain_window.get('end_time') is not None else '23:59'
+
     for dsn in dsns:
         print('*' * 100)
         print(dsn)
         print('*' * 100)
-        
+
+        # 如果启动时间不在维护时间范围内，则直接跳过
+        if datetime.now().strftime("%H:%M") < maintain_start_time or datetime.now().strftime("%H:%M") > maintain_end_time:
+            print(
+                'Start time is not within the maintenance time range, waiting for the next scheduling.')
+            continue
+
         try:
             counter = 0
             v = Vaccum(dsn)
@@ -169,15 +191,22 @@ def main(dsns=[]):
 
                 tbl_schema = table[0]
                 tbl_table = table[1]
+                tbl_dead_tup_ratio = table[4]
                 tbl_operation = table[5]
 
                 if tbl_operation == '':
                     continue
 
+                # 如果超过了一次维护窗口期，则需要跳出循环，等待下一次调度
+                if datetime.now().strftime("%H:%M") < maintain_start_time or datetime.now().strftime("%H:%M") > maintain_end_time:
+                    print(
+                        'The vacuum operation has exceeded the maintenance window, waiting for the next schedule.')
+                    continue
+
                 counter += 1
 
-                verbose = "[%d][%s] %s %s.%s" % (
-                    counter, datetime.utcnow(), tbl_operation, tbl_schema, tbl_table)
+                verbose = "[%d][%s] %s %s.%s (dead_tup_ratio:%s)" % (
+                    counter, datetime.now(), tbl_operation, tbl_schema, tbl_table, tbl_dead_tup_ratio)
                 print('=' * 100)
                 print(verbose)
 
@@ -201,7 +230,12 @@ def main(dsns=[]):
 if __name__ == '__main__':
 
     dsns = [
-        'postgresql://xxxx:xxxx@xxx:xxx/xxx?application_name=smart_vacuum',
+       'postgresql://xxxx:xxxx@xxx:xxx/xxx?application_name=smart_vacuum',
     ]
 
-    main(dsns)
+    maintain_window = {
+        'start_time': '01:00',
+        'end_time': '02:30'
+    }
+
+    main(dsns, maintain_window)
