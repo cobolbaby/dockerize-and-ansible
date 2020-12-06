a0eb09dcd1122c68c593e872872ac8d671a455db
Cobolbaby <Zhang.Xing-Long@inventec.com>
2020-12-06 19:20:38 +0800

    feat: pg all-in-one起头

---
+++ postgres/build/all-in-one/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# readonly PATRONI_SCOPE=${PATRONI_SCOPE:-batman}
+# PATRONI_NAMESPACE=${PATRONI_NAMESPACE:-/service}
+# readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
+# readonly DOCKER_IP=$(hostname --ip-address)
+
+# export PATRONI_SCOPE
+# export PATRONI_NAMESPACE
+# export PATRONI_NAME="${PATRONI_NAME:-$(hostname)}"
+# export PATRONI_RESTAPI_CONNECT_ADDRESS="$DOCKER_IP:8008"
+# export PATRONI_RESTAPI_LISTEN="0.0.0.0:8008"
+# export PATRONI_POSTGRESQL_CONNECT_ADDRESS="$DOCKER_IP:5432"
+# export PATRONI_POSTGRESQL_LISTEN="0.0.0.0:5432"
+# export PATRONI_POSTGRESQL_DATA_DIR="${PATRONI_POSTGRESQL_DATA_DIR:-$PGDATA}"
+# export PATRONI_REPLICATION_USERNAME="${PATRONI_REPLICATION_USERNAME:-replicator}"
+# export PATRONI_REPLICATION_PASSWORD="${PATRONI_REPLICATION_PASSWORD:-replicate}"
+# export PATRONI_SUPERUSER_USERNAME="${PATRONI_SUPERUSER_USERNAME:-postgres}"
+# export PATRONI_SUPERUSER_PASSWORD="${PATRONI_SUPERUSER_PASSWORD:-postgres}"
+
+# exec patroni /home/postgres/.config/patroni/patronictl.yaml
+
+# 获取启动模式，采用Patroni Or Not
+
+# 如果采用 Patroni 则执行 patroni + yaml
+
+# 如果是启动单点，则参考 PG 原始镜像的写法
+
+
