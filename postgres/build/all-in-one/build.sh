0611b117b8d71c68acb76b4b2c1137f0eed0457b
cobolbaby <cobolbaby@qq.com>
2020-12-08 13:46:57 +0800

    feat: 完善PG镜像(数据目录，配置文件，entrypoint)

--- postgres/build/all-in-one/build.sh
+++ postgres/build/all-in-one/build.sh
@@ -2,11 +2,11 @@
 set -e
 cd `dirname $0`
 
-PROXY=http://10.190.40.39:12317/
+PROXY=http://10.190.40.39:2379/
 
 docker build --rm -f Dockerfile \
-            -t registry.inventec/infra/postgres:11.8-partman-4.4.0 \
+            -t registry.inventec/infra/postgres:12.5 \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
             .
-docker push registry.inventec/infra/postgres:11.8-partman-4.4.0
+docker push registry.inventec/infra/postgres:12.5
--- postgres/build/all-in-one/entrypoint.sh
+++ postgres/build/all-in-one/entrypoint.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-# readonly PATRONI_SCOPE=${PATRONI_SCOPE:-batman}
-# PATRONI_NAMESPACE=${PATRONI_NAMESPACE:-/service}
-# readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
-# readonly DOCKER_IP=$(hostname --ip-address)
-
-# export PATRONI_SCOPE
-# export PATRONI_NAMESPACE
-# export PATRONI_NAME="${PATRONI_NAME:-$(hostname)}"
-# export PATRONI_RESTAPI_CONNECT_ADDRESS="$DOCKER_IP:8008"
-# export PATRONI_RESTAPI_LISTEN="0.0.0.0:8008"
-# export PATRONI_POSTGRESQL_CONNECT_ADDRESS="$DOCKER_IP:5432"
-# export PATRONI_POSTGRESQL_LISTEN="0.0.0.0:5432"
-# export PATRONI_POSTGRESQL_DATA_DIR="${PATRONI_POSTGRESQL_DATA_DIR:-$PGDATA}"
-# export PATRONI_REPLICATION_USERNAME="${PATRONI_REPLICATION_USERNAME:-replicator}"
-# export PATRONI_REPLICATION_PASSWORD="${PATRONI_REPLICATION_PASSWORD:-replicate}"
-# export PATRONI_SUPERUSER_USERNAME="${PATRONI_SUPERUSER_USERNAME:-postgres}"
-# export PATRONI_SUPERUSER_PASSWORD="${PATRONI_SUPERUSER_PASSWORD:-postgres}"
-
-# exec patroni /home/postgres/.config/patroni/patronictl.yaml
-
 # 获取启动模式，采用Patroni Or Not
 
 # 如果采用 Patroni 则执行 patroni + yaml
 
 # 如果是启动单点，则参考 PG 原始镜像的写法
+readonly PATRONI_ENABLE=${PATRONI_ENABLE:-false}
 
+if [ "$PATRONI_ENABLE" != "true" ]
+then
+    echo "Start a single instance..."
+    /docker-entrypoint.sh "$@"
+    exit
+fi
 
+echo "Start Patroni..."
+
+readonly PATRONI_SCOPE=${PATRONI_SCOPE:-patroni}
+PATRONI_NAMESPACE=${PATRONI_NAMESPACE:-/pgcluster}
+# Ref: https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
+# Remove Pattern (Back of $VAR)
+readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
+readonly DOCKER_IP=$(hostname --ip-address)
+
+export PATRONI_NAMESPACE
+export PATRONI_SCOPE
+export PATRONI_NAME="${PATRONI_NAME:-$(hostname)}"
+export PATRONI_RESTAPI_LISTEN="0.0.0.0:8008"
+export PATRONI_RESTAPI_CONNECT_ADDRESS="$DOCKER_IP:8008"
+export PATRONI_POSTGRESQL_LISTEN="0.0.0.0:5432"
+export PATRONI_POSTGRESQL_CONNECT_ADDRESS="$DOCKER_IP:5432"
+export PATRONI_POSTGRESQL_DATA_DIR="${PATRONI_POSTGRESQL_DATA_DIR:-$PGDATA}"
+export PATRONI_REPLICATION_USERNAME="${PATRONI_REPLICATION_USERNAME:-replicator}"
+export PATRONI_REPLICATION_PASSWORD="${PATRONI_REPLICATION_PASSWORD:-replicate}"
+export PATRONI_SUPERUSER_USERNAME="${PATRONI_SUPERUSER_USERNAME:-admin123}"
+export PATRONI_SUPERUSER_PASSWORD="${PATRONI_SUPERUSER_PASSWORD:-admin123}"
+
+exec patroni /home/postgres/.config/patroni/patronictl.yaml
