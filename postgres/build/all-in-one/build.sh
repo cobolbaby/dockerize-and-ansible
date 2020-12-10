a71a285794556a41a674a43ced67026a7737c9e8
cobolbaby <cobolbaby@qq.com>
2020-12-10 16:02:53 +0800

    chore: 调整PG镜像构建脚本

--- postgres/build/all-in-one/build.sh
+++ postgres/build/all-in-one/build.sh
@@ -3,10 +3,12 @@ set -e
 cd `dirname $0`
 
 PROXY=http://10.190.40.39:2379/
+PG_MAJOR=12.5
 
 docker build --rm -f Dockerfile \
-            -t registry.inventec/infra/postgres:12.5 \
+            -t registry.inventec/infra/postgres:${PG_MAJOR} \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
+            --build-arg PG_MAJOR=${PG_MAJOR} \
             .
-docker push registry.inventec/infra/postgres:12.5
+docker push registry.inventec/infra/postgres:${PG_MAJOR}
