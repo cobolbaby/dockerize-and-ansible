4ed89ab2c74394fa747b00587e76d9a834161e64
cobolbaby <cobolbaby@qq.com>
2021-04-12 19:23:24 +0800

    optimize: 利用pgAdmin4提供的util去完成模板路径拼接

--- pgadmin4/launch.sh
+++ pgadmin4/launch.sh
@@ -15,14 +15,6 @@ sudo chown -R 5050:5050 /opt/pgadmin4/pgadmin
 # make docker
 
 docker run -p 80:80 \
-    --name pgadmin4 \
-    -v /opt/pgadmin4/pgadmin:/var/lib/pgadmin \
-    -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
-    -e "PGADMIN_DEFAULT_PASSWORD=123456" \
-    --restart always \
-    -d dpage/pgadmin4:4.29
-
-docker run -p 80:80 \
     --name pgadmin5 \
     -v /opt/pgadmin4/pgadmin5:/var/lib/pgadmin \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
