54d3b912be8c35d43c1dee5f1d57cfe7b1842eea
cobolbaby <cobolbaby@qq.com>
2022-04-01 10:00:51 +0800

    fix: pgpool.tmpl 中配置动态注入

--- pgpool/bin/entrypoint.sh
+++ pgpool/bin/entrypoint.sh
@@ -7,14 +7,14 @@ readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
 readonly PGPOOL_HEALTH_CHECK_USER=${PGPOOL_HEALTH_CHECK_USER:-nobody}
 readonly PGPOOL_HEALTH_CHECK_PASSWORD=${PGPOOL_HEALTH_CHECK_PASSWORD:-}
 
+# Dynamic configuration 
+sed -i "s/^#*\s*health_check_user\s*=.*/health_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
+sed -i "s/^#*\s*health_check_password\s*=.*/health_check_password = '${PGPOOL_HEALTH_CHECK_PASSWORD}'/" /etc/confd/templates/pgpool.tmpl
+
 # Start Confd
 CONFD="confd -prefix=$PATRONI_NAMESPACE/$PATRONI_SCOPE -interval=10 -backend"
 
 $CONFD etcd -node $(echo $ETCDCTL_ENDPOINTS | sed 's/,/ -node /g') -onetime -sync-only
-
-# Dynamic configuration 
-sed -i "s/^#*\s*health_check_user\s*=.*/health_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" ${PGPOOL_INSTALL_DIR}/etc/pgpool.conf
-sed -i "s/^#*\s*health_check_password\s*=.*/health_check_password = '${PGPOOL_HEALTH_CHECK_PASSWORD}'/" ${PGPOOL_INSTALL_DIR}/etc/pgpool.conf
 
 # Start Pgpool-II
 ${PGPOOL_INSTALL_DIR}/bin/pgpool \
