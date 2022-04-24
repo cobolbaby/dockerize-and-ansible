ccdf049a4358be6676da4a73c6f59c1d45784b0c
cobolbaby <cobolbaby@qq.com>
2022-04-24 15:00:48 +0800

    feat(harbor): upgrade script

--- harbor/upgrade.sh
+++ harbor/upgrade.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mv /opt/harbor /opt/harbor_v1.9.4
-sudo cp -r /data/database /opt/harbor_v1.9.4_database
-tar -zxvf ~/harbor-offline-installer-v2.0.6.tgz -C /opt
-cd /opt/harbor
-docker load -i harbor.v2.0.6.tar.gz
-cp /opt/harbor_v1.9.4/harbor.yml .
-docker run -it --rm -v /:/hostfs goharbor/prepare:v2.0.6 migrate -i /opt/harbor/harbor.yml
-./install.sh
-docker-compose up -d
+# mv /opt/harbor /opt/harbor_v1.9.4
+# sudo cp -r /data/database /opt/harbor_v1.9.4_database
+# tar -zxvf ~/harbor-offline-installer-v2.0.6.tgz -C /opt
+# cd /opt/harbor
+# docker load -i harbor.v2.0.6.tar.gz
+# cp /opt/harbor_v1.9.4/harbor.yml .
+# docker run -it --rm -v /:/hostfs goharbor/prepare:v2.0.6 migrate -i /opt/harbor/harbor.yml
+# ./install.sh
+# docker-compose up -d
 
 # mv /opt/harbor /opt/harbor_v2.0.6
 # sudo cp -r /data/database /opt/harbor_v2.0.6_database
@@ -19,3 +19,13 @@ docker-compose up -d
 # docker run -it --rm -v /:/hostfs goharbor/prepare:v2.1.4 migrate -i /opt/harbor/harbor.yml
 # ./install.sh
 # docker-compose up -d
+
+mv /opt/harbor /opt/harbor_v2.1.4
+sudo cp -r /data/database /opt/harbor_v2.1.4_database
+tar -zxvf ~/harbor-offline-installer-v2.3.3.tgz -C /opt
+cd /opt/harbor
+docker load -i harbor.v2.3.3.tar.gz
+cp /opt/harbor_v2.1.4/harbor.yml .
+docker run -it --rm -v /:/hostfs goharbor/prepare:v2.3.3 migrate -i /opt/harbor/harbor.yml
+./install.sh
+docker-compose up -d
