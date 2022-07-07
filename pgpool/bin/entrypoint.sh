37a1bb1161024c3d00d97a0445794c38aed17496
cobolbaby <cobolbaby@qq.com>
2022-07-07 10:43:57 +0800

    feat(pgpool): support etcdv3

--- pgpool/bin/entrypoint.sh
+++ pgpool/bin/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-readonly PATRONI_SCOPE=${PATRONI_SCOPE:-batman}
 PATRONI_NAMESPACE=${PATRONI_NAMESPACE:-/service}
 # rtrim /
 readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
+readonly PATRONI_SCOPE=${PATRONI_SCOPE:-batman}
 readonly PGPOOL_HEALTH_CHECK_USER=${PGPOOL_HEALTH_CHECK_USER:-nobody}
 readonly PGPOOL_HEALTH_CHECK_PASSWORD=${PGPOOL_HEALTH_CHECK_PASSWORD:-}
+readonly CONFD_BACKEND=${CONFD_BACKEND:-etcd}
 
 # Dynamic configuration 
 sed -i "s/^#*\s*health_check_user\s*=.*/health_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
@@ -14,13 +15,13 @@ sed -i "s/^#*\s*sr_check_user\s*=.*/sr_check_user = '${PGPOOL_HEALTH_CHECK_USER}
 sed -i "s/^#*\s*sr_check_password\s*=.*/sr_check_password = '${PGPOOL_HEALTH_CHECK_PASSWORD}'/" /etc/confd/templates/pgpool.tmpl
 
 # Start Confd
-CONFD="confd -prefix=$PATRONI_NAMESPACE/$PATRONI_SCOPE -interval=10 -backend"
+CONFD="confd -prefix=${PATRONI_NAMESPACE}/${PATRONI_SCOPE} -interval=10"
 
-$CONFD etcd -node $(echo $ETCDCTL_ENDPOINTS | sed 's/,/ -node /g') -onetime -sync-only
+$CONFD -backend $CONFD_BACKEND -node $(echo $ETCDCTL_ENDPOINTS | sed 's/,/ -node /g') -onetime -sync-only
 
 # Start Pgpool-II
 ${PGPOOL_INSTALL_DIR}/bin/pgpool \
     -f ${PGPOOL_INSTALL_DIR}/etc/pgpool.conf \
     -F ${PGPOOL_INSTALL_DIR}/etc/pcp.conf
 
-$CONFD etcd -node $(echo $ETCDCTL_ENDPOINTS | sed 's/,/ -node /g')
+$CONFD -backend $CONFD_BACKEND -node $(echo $ETCDCTL_ENDPOINTS | sed 's/,/ -node /g')
