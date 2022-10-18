344f74601bf0ae3ebe80ea367729daf15d5f5086
cobolbaby <cobolbaby@qq.com>
2022-10-18 15:53:19 +0800

    feat(pgpool): pool_passwd动态注入

--- pgpool/bin/entrypoint.sh
+++ pgpool/bin/entrypoint.sh
@@ -9,10 +9,8 @@ readonly PGPOOL_HEALTH_CHECK_PASSWORD=${PGPOOL_HEALTH_CHECK_PASSWORD:-}
 readonly CONFD_BACKEND=${CONFD_BACKEND:-etcd}
 
 # Dynamic configuration 
-# sed -i "s/^#*\s*health_check_user\s*=.*/health_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
-# sed -i "s/^#*\s*health_check_password\s*=.*/health_check_password = '${PGPOOL_HEALTH_CHECK_PASSWORD}'/" /etc/confd/templates/pgpool.tmpl
 sed -i "s/^#*\s*sr_check_user\s*=.*/sr_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
-sed -i "s/^#*\s*sr_check_password\s*=.*/sr_check_password = '${PGPOOL_HEALTH_CHECK_PASSWORD}'/" /etc/confd/templates/pgpool.tmpl
+echo "${PGPOOL_HEALTH_CHECK_USER}:md5${PGPOOL_HEALTH_CHECK_PASSWORD}" > ${PGPOOL_INSTALL_DIR}/etc/pool_passwd
 
 # Start Confd
 CONFD="confd -prefix=${PATRONI_NAMESPACE}/${PATRONI_SCOPE} -interval=10"
