4edc8d29b523912167dc84e4f8a0d6574d0c8336
cobolbaby <cobolbaby@qq.com>
2023-01-31 10:11:02 +0800

    feat(toolbox): 定制 Toolbox，方便debug容器问题

---
+++ toolbox/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+cd `dirname $0`
+
+PROXY=http://10.190.81.209:3389/
+VERSION=latest
+GO_VERSION=1.19.4
+
+docker build --rm -f Dockerfile \
+            -t registry.inventec/infra/debugger:${VERSION} \
+            --build-arg http_proxy=${PROXY} \
+            --build-arg https_proxy=${PROXY} \
+            --build-arg GO_VERSION=${GO_VERSION} \
+            .
+
+# docker run -it --rm --name toolbox --net host registry.inventec/infra/debugger /bin/bash
+# go tool pprof -http :??? http://localhost:6060/debug/pprof/heap
