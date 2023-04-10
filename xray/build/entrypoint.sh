a30a96599358edbcec7341af295b05e01623d969
cobolbaby <cobolbaby@qq.com>
2023-04-10 18:27:33 +0800

    opti(xray): nginx not serve tls port

--- xray/build/entrypoint.sh
+++ xray/build/entrypoint.sh
@@ -13,7 +13,10 @@ CERTBOT_SSL_KEY_PATH="/etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem"
 # Path to the Nginx SSL configuration file
 NGINX_SSL_CONF="/etc/nginx/conf.d/${CERTBOT_DOMAIN}.conf"
 
-XRAY_CONF="/opt/xray/config.json"
+# Update the Nginx SSL configuration file with the paths to the SSL certificate and key
+cp /etc/nginx/conf.d/example.org.conf.tmpl $NGINX_SSL_CONF
+
+sed -i "s|example.org|$CERTBOT_DOMAIN|g" $NGINX_SSL_CONF
 
 # Start nginx in the background
 nginx 
@@ -25,19 +28,15 @@ if [[ ! -f $CERTBOT_SSL_CERT_PATH || ! -f $CERTBOT_SSL_KEY_PATH ]]; then
     certbot certonly --non-interactive --agree-tos --email $CERTBOT_EMAIL --webroot --webroot-pat /usr/share/nginx/html -d $CERTBOT_DOMAIN
 fi
 
-# Update the Nginx SSL configuration file with the paths to the SSL certificate and key
-cp /etc/nginx/conf.d/example.org.conf.tmpl $NGINX_SSL_CONF
+# sed -i "s|# ssl_certificate .*|ssl_certificate $CERTBOT_SSL_CERT_PATH;|" $NGINX_SSL_CONF
+# sed -i "s|# ssl_certificate_key .*|ssl_certificate_key $CERTBOT_SSL_KEY_PATH;|" $NGINX_SSL_CONF
 
-sed -i "s|example.org|$CERTBOT_DOMAIN|g" $NGINX_SSL_CONF
-sed -i "s|ssl_certificate .*|ssl_certificate $CERTBOT_SSL_CERT_PATH;|" $NGINX_SSL_CONF
-sed -i "s|ssl_certificate_key .*|ssl_certificate_key $CERTBOT_SSL_KEY_PATH;|" $NGINX_SSL_CONF
+# nginx -t && nginx -s reload
 
-# Check Nginx Configuration
-nginx -t 
+# sleep 5
 
-nginx -s reload
-
-sleep 5
+# Start XRAY
+XRAY_CONF="/opt/xray/config.json"
 
 sed -i "s|\"id\": \"\"|\"id\": \"$(xray uuid)\"|" $XRAY_CONF
 sed -i "s|\"certificateFile\": \".*\"|\"certificateFile\": \"$CERTBOT_SSL_CERT_PATH\"|" $XRAY_CONF
