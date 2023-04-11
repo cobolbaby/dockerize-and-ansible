46d1ec599440223710362c37987fc24f06bbe7e0
cobolbaby <cobolbaby@qq.com>
2023-04-11 12:30:20 +0800

    feat(postgres): PG HA 的部署脚本最简化 ，减少对 Swarm/Overlay的依赖

--- postgres/deploy.dev.sh
+++ postgres/deploy.dev.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 cd `dirname $0`
 
-# docker swarm init --advertise-addr 10.191.7.12
-# docker swarm join --token SWMTKN-1-14hlcs07jekxx3260bu1q42s7lwa8nvybz4e4eu4ey4qzz59ay-bac89hu6qm8z9d07qkvoqgxq8 10.191.7.12:2377
-# docker node promote kubernetes-4
-# docker node promote kubernetes-5
-# docker node update --label-add alias=bdc06.infra.dev.itc.inventec kubernetes-2
-# docker node update --label-add alias=bdc07.infra.dev.itc.inventec kubernetes-4
-# docker node update --label-add alias=bdc08.infra.dev.itc.inventec kubernetes-5
-# docker network create --driver overlay --subnet=10.13.0.0/16 --attachable bdc
+# 前置操作:
+# docker network create --driver bridge --subnet=10.14.0.0/16 infra
 
 INVENTORY_FILE=../inventory.dev
 
-# ansible -i $INVENTORY_FILE patroni1 -m raw -a "docker stack rm postgres"
-# ansible -i $INVENTORY_FILE patroni1 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-master.yml down"
-# ansible -i $INVENTORY_FILE patroni2 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-standby.yml down"
-# ansible -i $INVENTORY_FILE patroni3 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-standby2.yml down"
-# exit
-
 # 创建数据目录
-ansible -i $INVENTORY_FILE patroni -m file -a "dest=/data/hdd/pg/12/data owner=999 group=999 mode=700 state=directory" -b
-ansible -i $INVENTORY_FILE patroni -m file -a "dest=/data/ssd/pg/12 owner=999 group=999 mode=700 state=directory" -b
-ansible -i $INVENTORY_FILE patroni -m file -a "dest=/data/ssd/pg/12/data owner=999 group=999 mode=700 state=directory" -b
+ansible -i $INVENTORY_FILE postgres -m file -a "dest=/data/hdd/pg/12/data owner=999 group=999 mode=700 state=directory" -b
+ansible -i $INVENTORY_FILE postgres -m file -a "dest=/data/ssd/pg/12/data owner=999 group=999 mode=700 state=directory" -b
 
-# 推送启动脚本至部署目录
 # 传出配置文件
-ansible -i $INVENTORY_FILE patroni -m copy -a "src=deploy.dev/ dest=/opt/patroni"
+ansible -i $INVENTORY_FILE postgres -m copy -a "src=deploy.dev/ dest=/opt/postgres"
 
 # 执行启动命令
-ansible -i $INVENTORY_FILE patroni1 -m raw -a "chmod +x /opt/patroni/bin/start.sh"
-ansible -i $INVENTORY_FILE patroni1 -m raw -a "/opt/patroni/bin/start.sh"
-# exit
+ansible -i $INVENTORY_FILE pg01 -m raw -a "cd /opt/postgres/bin && docker-compose -f docker-compose-pg01.yml up -d"
+ansible -i $INVENTORY_FILE pg02 -m raw -a "cd /opt/postgres/bin && docker-compose -f docker-compose-pg02.yml up -d"
 # Keepalived
-ansible -i $INVENTORY_FILE patroni -m raw -a "chmod +x /opt/patroni/keepalived/*/*.sh"
-ansible -i $INVENTORY_FILE patroni -m raw -a "chmod 644 /opt/patroni/keepalived/*/*.conf"
-# ansible -i $INVENTORY_FILE patroni -m raw -a "docker pull registry.inventec/infra/keepalived:2.0.17" -f 5
-ansible -i $INVENTORY_FILE patroni1 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-master.yml up -d"
-ansible -i $INVENTORY_FILE patroni2 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-standby.yml up -d"
-ansible -i $INVENTORY_FILE patroni3 -m raw -a "docker-compose -f /opt/patroni/keepalived/docker-compose-keepalived-standby2.yml up -d"
+ansible -i $INVENTORY_FILE postgres -m raw -a "chmod +x /opt/postgres/keepalived/*/*.sh" -b
+ansible -i $INVENTORY_FILE pg01 -m raw -a "cd /opt/postgres/keepalived && docker-compose -f docker-compose-keepalived-master.yml up -d"
+ansible -i $INVENTORY_FILE pg02 -m raw -a "cd /opt/postgres/keepalived && docker-compose -f docker-compose-keepalived-standby.yml up -d"
--- postgres/deploy.dev/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd `dirname $0`
-
-docker stack deploy -c docker-compose-pg12.yml postgres
