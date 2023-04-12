7840be634a5dc34c4e57ab123da4726f9c023ff9
cobolbaby <cobolbaby@qq.com>
2023-04-12 17:45:48 +0800

    feat(dkron): base env upgrade

--- dkron/build.sh
+++ dkron/build.sh
@@ -2,11 +2,22 @@
 set -e
 cd `dirname $0`
 
+[[ $# -lt 1 ]] && {
+  echo "Usage: $0 proxy:port"
+  exit 1
+}
+
+proxy=$1
+
 REGISTRY=registry.inventec
-DKRON_VERSION=1.2.5
+DKRON_VERSION=3.2.3
+OPENJDK_VERSION=8u342-jre-slim-buster
 
 docker build --rm -f Dockerfile \
-            -t ${REGISTRY}/infra/dkron:${DKRON_VERSION} \
+            -t ${REGISTRY}/infra/dkron-executor-docker:${DKRON_VERSION} \
+            --build-arg http_proxy=http://${proxy} \
+            --build-arg https_proxy=http://${proxy} \
             --build-arg DKRON_VERSION=${DKRON_VERSION} \
+            --build-arg OPENJDK_VERSION=${OPENJDK_VERSION} \
             .
-docker push ${REGISTRY}/infra/dkron:${DKRON_VERSION}
+docker push ${REGISTRY}/infra/dkron-executor-docker:${DKRON_VERSION} 
