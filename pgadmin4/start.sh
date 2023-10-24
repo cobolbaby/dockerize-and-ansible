e582f00d583237cce38d17d725f1d3f5dc9f42c2
cobolbaby <cobolbaby@qq.com>
2023-10-24 17:01:34 +0800

    fix(pgadmin): start script

--- pgadmin4/start.sh
+++ pgadmin4/start.sh
@@ -3,7 +3,7 @@
 # https://www.pgadmin.org/docs/pgadmin4/latest/container_deployment.html
 # /pgadmin4 $ id
 # uid=5050(pgadmin) gid=5050(pgadmin)
-sudo chown -R 5050:5050 /tmp/pgadmin
+sudo chown -R 5050:5050 /data/pgadmin4/pgadmin
 
 # git clone https://github.com/pgadmin-org/pgadmin4.git
 # cd pgadmin4
@@ -11,10 +11,24 @@ sudo chown -R 5050:5050 /tmp/pgadmin
 # git apply gpdb6-support.patch
 # make docker
 
-docker run -p 80:80 \
-    --name pgadmin5 \
-    -v /tmp/pgadmin:/var/lib/pgadmin \
+docker run -d --name pgadmin5 \
+    -p 80:80 \
+    -v /data/pgadmin4/pgadmin:/var/lib/pgadmin \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=123456" \
     --restart always \
-    -d registry.inventec/infra/dpage/pgadmin4:5.7
+    registry.inventec/infra/dpage/pgadmin4:5.7.1
+
+<< comment
+
+sudo chown -R 5050:5050 /data/pgadmin4/pgadmin6
+
+docker run -d --name pgadmin6 \
+    -p 81:80 \
+    -v /data/pgadmin4/pgadmin6:/var/lib/pgadmin \
+    -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
+    -e "PGADMIN_DEFAULT_PASSWORD=123456" \
+    --restart always \
+    registry.inventec/proxy/dpage/pgadmin4:latest
+
+comment
