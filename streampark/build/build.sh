9ab34de28ebfe44aac44599c41ee904e13cadf87
cobolbaby <cobolbaby@qq.com>
2023-12-28 15:40:11 +0800

    chore(streampark): build script

--- streampark/build/build.sh
+++ streampark/build/build.sh
@@ -3,8 +3,10 @@ set -e
 cd `dirname $0`
 
 REGISTRY=registry.inventec
+STREAMPARK_VERSION=v2.1.2
 
 docker build --rm -f Dockerfile \
-            -t ${REGISTRY}/infra/streampark:latest \
+            -t ${REGISTRY}/infra/streampark:${STREAMPARK_VERSION} \
+            --build-arg STREAMPARK_VERSION=${STREAMPARK_VERSION} \
             .
-docker push ${REGISTRY}/infra/streampark:latest 
+docker push ${REGISTRY}/infra/streampark:${STREAMPARK_VERSION} 
