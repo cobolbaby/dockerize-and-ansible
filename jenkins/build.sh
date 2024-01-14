61506c4eb1b2aeb6b73efbdbf3c801a121b61b53
cobolbaby <cobolbaby@qq.com>
2024-01-14 11:31:47 +0800

    fix(jenkins): docker image namespace

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -7,9 +7,9 @@ PROXY=http://10.190.40.39:12306/
 VERSION=2.204.1-centos
 
 docker build --rm -f Dockerfile \
-            -t ${REGISTRY}/development/jenkins:${VERSION} \
+            -t ${REGISTRY}/infra/jenkins:${VERSION} \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
             --build-arg VERSION=${VERSION} \
             .
-docker push ${REGISTRY}/development/jenkins:${VERSION}
+docker push ${REGISTRY}/infra/jenkins:${VERSION}
