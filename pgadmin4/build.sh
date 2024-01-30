2cee724e78dbbfda1f6cc74a8a3279d939e29da5
cobolbaby <cobolbaby@qq.com>
2024-01-30 15:29:34 +0800

    fix(pgadmin): GUNICORN_THREADS + build script

--- pgadmin4/build.sh
+++ pgadmin4/build.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 cd `dirname $0`
 
-PROXY=http://10.190.41.209:3388/
+PROXY=http://10.190.81.209:3389/
 VERSION=5.7
+
+# git clone https://github.com/pgadmin-org/pgadmin4.git
+# cd pgadmin4
+# git checkout REL-5_7
+# git apply gpdb6-support.patch
+# make docker
 
 docker build --rm -f Dockerfile \
             -t registry.inventec/infra/dpage/pgadmin4:${VERSION} \
--- pgadmin4/start.sh
+++ pgadmin4/start.sh
@@ -5,27 +5,22 @@
 # uid=5050(pgadmin) gid=5050(pgadmin)
 sudo chown -R 5050:5050 /data/pgadmin4/pgadmin
 
-# git clone https://github.com/pgadmin-org/pgadmin4.git
-# cd pgadmin4
-# git checkout REL-5_7
-# git apply gpdb6-support.patch
-# make docker
-
 docker run -d --name pgadmin5 \
     -p 80:80 \
     -v /data/pgadmin4/pgadmin:/var/lib/pgadmin \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=123456" \
+    -e "GUNICORN_THREADS=50" \
     --restart always \
     registry.inventec/infra/dpage/pgadmin4:5.7.1
 
 << comment
 
-sudo chown -R 5050:5050 /data/pgadmin4/pgadmin6
+sudo chown -R 5050:5050 /data/pgadmin4/pgadmin8
 
-docker run -d --name pgadmin6 \
+docker run -d --name pgadmin8 \
     -p 81:80 \
-    -v /data/pgadmin4/pgadmin6:/var/lib/pgadmin \
+    -v /data/pgadmin4/pgadmin8:/var/lib/pgadmin \
     -e "PGADMIN_DEFAULT_EMAIL=cobolbaby@qq.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=123456" \
     --restart always \
