5bbb838ef5c96e6b9131a765aba4f8f513337617
cobolbaby <cobolbaby@qq.com>
2024-03-22 17:36:29 +0800

    chore(kafka-connect): 更新 build 脚本

--- kafka/kafka-connect/build.sh
+++ kafka/kafka-connect/build.sh
@@ -2,9 +2,14 @@
 set -e
 cd `dirname $0`
 
-REGISTRY=registry.inventec
-TAGNAME=development/cp-kafka-connect:5.3.1-p3
-PROXY=http://10.190.40.39:12306/
+REGISTRY=registry.inventec/infra
+TAG=7.5.3
+PROXY=http://10.190.81.209:3389/
 
-docker build --rm -f Dockerfile -t ${REGISTRY}/${TAGNAME} --build-arg http_proxy=${PROXY} --build-arg https_proxy=${PROXY} .
-docker push ${REGISTRY}/$TAGNAME
+docker build --rm -f Dockerfile \
+            -t ${REGISTRY}/confluentinc/cp-kafka-connect:${TAG} \
+            --build-arg TAG=${TAG} \
+            --build-arg http_proxy=${PROXY} \
+            --build-arg https_proxy=${PROXY} \
+            .
+docker push ${REGISTRY}/confluentinc/cp-kafka-connect:${TAG}
