fb0d6da2d5093475e931756dceeb1b436b85a57f
cobolbaby <cobolbaby@qq.com>
2024-09-20 16:42:30 +0800

    chore(postgres): bump latest influxdb_fdw and pg_analytics

--- postgres/build.sh
+++ postgres/build.sh
@@ -7,10 +7,21 @@ PG_VERSION=16.4
 
 cd build
 
-docker build --rm -f Dockerfile \
+# docker buildx rm container-builder
+
+# docker buildx create \
+#   --name container-builder \
+#   --driver docker-container \
+#   --use --bootstrap
+
+# 遇到了 registry.inventec 无法解析和证书的问题
+# https://github.com/docker/buildx/issues/835
+
+docker buildx build --rm -f Dockerfile \
             -t registry.inventec/infra/postgres:${PG_VERSION} \
             --build-arg PG_VERSION=${PG_VERSION} \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
             .
+            # --platform linux/amd64,linux/arm64 \
 docker push registry.inventec/infra/postgres:${PG_VERSION}
