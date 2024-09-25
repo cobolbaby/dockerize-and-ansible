c845bf9fae9362a873af84aaf379afc1bb9de439
cobolbaby <cobolbaby@qq.com>
2024-09-26 01:16:40 +0800

    chore(dts): bump seatunnel 2.3.7

--- dts/seatunnel/build.sh
+++ dts/seatunnel/build.sh
@@ -2,13 +2,13 @@
 cd `dirname $0`
 
 PROXY=http://10.190.81.209:3389/
-SEATUNNEL_VERSION=2.3.5
+SEATUNNEL_VERSION=2.3.7
 
 docker build --rm -f Dockerfile \
             -t registry.inventec/infra/seatunnel:${SEATUNNEL_VERSION} \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
-            --build-arg no_proxy=localhost,127.0.0.1,*.itc.inventec.net \
+            --build-arg no_proxy=localhost,127.0.0.1,nexus.itc.inventec.net \
             --build-arg SEATUNNEL_VERSION=${SEATUNNEL_VERSION} \
             .
 
