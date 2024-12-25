96868352d8fd3c357b647bfdb312349bf5d93f6b
cobolbaby <cobolbaby@qq.com>
2024-12-25 15:16:57 +0800

    chore(toolbox): bump pg_dump

--- toolbox/build.sh
+++ toolbox/build.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 
 PROXY=http://10.190.81.209:3389/
 VERSION=latest
-GO_VERSION=1.22.8
+GO_VERSION=1.22.10
 
 docker build --rm -f Dockerfile \
             -t registry.inventec/infra/debugger:${VERSION} \
@@ -12,5 +12,7 @@ docker build --rm -f Dockerfile \
             --build-arg GO_VERSION=${GO_VERSION} \
             .
 
-# docker run -it --rm --name toolbox --net host registry.inventec/infra/debugger /bin/bash
+# docker run -it --rm --net host registry.inventec/infra/debugger /bin/bash
 # go tool pprof -http :??? http://localhost:6060/debug/pprof/heap
+
+# docker run -it --rm registry.inventec/infra/debugger pg_dump --version
