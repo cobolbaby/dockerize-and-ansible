336084959bb98c13ed6219cbef3f3a85b5e8bd24
cobolbaby <cobolbaby@qq.com>
2025-04-02 12:32:58 +0800

    feat(pgpool): 1. 支持PGPOOL_NUM_INIT_CHILDREN; 2. 根据最新配置文件模版微调默认配置 3. 移除冗余的配置

--- pgpool/build/bin/entrypoint.sh
+++ pgpool/build/bin/entrypoint.sh
@@ -4,16 +4,18 @@ PATRONI_NAMESPACE=${PATRONI_NAMESPACE:-/service}
 # rtrim /
 readonly PATRONI_NAMESPACE=${PATRONI_NAMESPACE%/}
 readonly PATRONI_SCOPE=${PATRONI_SCOPE:-batman}
+readonly CONFD_BACKEND=${CONFD_BACKEND:-etcd}
 readonly PGPOOL_HEALTH_CHECK_USER=${PGPOOL_HEALTH_CHECK_USER:-nobody}
 readonly PGPOOL_HEALTH_CHECK_PASSWORD=${PGPOOL_HEALTH_CHECK_PASSWORD:-}
 readonly PGPOOL_PORT=${PGPOOL_PORT:-5432}
-readonly CONFD_BACKEND=${CONFD_BACKEND:-etcd}
+readonly PGPOOL_NUM_INIT_CHILDREN=${PGPOOL_NUM_INIT_CHILDREN:-200}
 
 # Dynamic configuration 
 sed -i "s/^#*\s*sr_check_user\s*=.*/sr_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
 sed -i "s/^#*\s*health_check_user\s*=.*/health_check_user = '${PGPOOL_HEALTH_CHECK_USER}'/" /etc/confd/templates/pgpool.tmpl
 sed -i "s/^#*\s*port\s*=.*/port = ${PGPOOL_PORT}/" /etc/confd/templates/pgpool.tmpl
-echo "${PGPOOL_HEALTH_CHECK_USER}:md5${PGPOOL_HEALTH_CHECK_PASSWORD}" > ${PGPOOL_INSTALL_DIR}/etc/pool_passwd
+sed -i "s/^#*\s*num_init_children\s*=.*/num_init_children = ${PGPOOL_NUM_INIT_CHILDREN}/" /etc/confd/templates/pgpool.tmpl
+echo "${PGPOOL_HEALTH_CHECK_USER}:TEXT${PGPOOL_HEALTH_CHECK_PASSWORD}" > ${PGPOOL_INSTALL_DIR}/etc/pool_passwd
 
 # Start Confd
 CONFD="confd -prefix=${PATRONI_NAMESPACE}/${PATRONI_SCOPE} -interval=10"
