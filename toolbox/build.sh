bca0088239022194b62009007631902845d127cb
cobolbaby <cobolbaby@qq.com>
2025-05-06 14:11:39 +0800

    chore(toolbox): bump mat/openjdk/smartctl/ ssacli

--- toolbox/build.sh
+++ toolbox/build.sh
@@ -3,13 +3,15 @@ cd `dirname $0`
 
 PROXY=http://10.190.81.209:3389/
 VERSION=latest
-GO_VERSION=1.22.10
+GO_VERSION=1.24.2
+MAT_VERSION=1.16.1.20250109
 
 docker build --rm -f Dockerfile \
             -t registry.inventec/infra/debugger:${VERSION} \
             --build-arg http_proxy=${PROXY} \
             --build-arg https_proxy=${PROXY} \
             --build-arg GO_VERSION=${GO_VERSION} \
+            --build-arg MAT_VERSION=${MAT_VERSION} \
             .
 
 # docker run -it --rm --net host registry.inventec/infra/debugger /bin/bash
