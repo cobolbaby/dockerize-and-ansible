a7bf745043177b6061997324fce9c0649a71b7af
cobolbaby <cobolbaby@qq.com>
2025-06-10 19:52:10 +0800

    feat(prometheus): 实现 alertmanager-webhook，矫正源端系统产生的可能异常的数据

--- prometheus/proxy-webhook/go.mod
+++ prometheus/proxy-webhook/go.mod
@@ -1,3 +1,22 @@
 module proxy-webhook
 
 go 1.24.2
+
+require (
+	github.com/prometheus/alertmanager v0.28.1
+	github.com/prometheus/common v0.64.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_golang v1.20.5 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/shurcooL/httpfs v0.0.0-20230704072500-f1e31cf0ba5c // indirect
+	github.com/shurcooL/vfsgen v0.0.0-20230704071429-0000e147ea92 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
--- prometheus/proxy-webhook/main.go
+++ prometheus/proxy-webhook/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -8,13 +10,53 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
 	"sync/atomic"
+	"time"
+
+	"github.com/prometheus/alertmanager/template"
+	"github.com/prometheus/common/model"
 )
+
+type FingerprintStore struct {
+	mu    sync.RWMutex
+	store map[model.Fingerprint]time.Time
+}
+
+func NewFingerprintStore() *FingerprintStore {
+	return &FingerprintStore{
+		store: make(map[model.Fingerprint]time.Time),
+	}
+}
+
+func (s *FingerprintStore) Get(fp model.Fingerprint) (time.Time, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	t, ok := s.store[fp]
+	return t, ok
+}
+
+func (s *FingerprintStore) SetIfAbsent(fp model.Fingerprint, t time.Time) time.Time {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if existing, ok := s.store[fp]; ok {
+		return existing
+	}
+	s.store[fp] = t
+	return t
+}
+
+func (s *FingerprintStore) Delete(fp model.Fingerprint) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.store, fp)
+}
 
 var (
 	targets    []*url.URL
 	counter    uint64
 	listenPort string
+	store      *FingerprintStore = NewFingerprintStore()
 )
 
 type multiFlag []string
@@ -78,14 +120,116 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 
 	proxyURL := target.ResolveReference(r.URL)
 
-	proxyReq, err := http.NewRequest(r.Method, proxyURL.String(), r.Body)
+	/*
+	   2025/05/28 10:28:53 ======= HTTP 请求开始 =======
+	   2025/05/28 10:28:53 POST / HTTP/1.1
+	   Host: 192.168.2.17:9094
+	   Content-Length: 953
+	   Content-Type: application/json
+	   User-Agent: Alertmanager/0.24.0
+
+	   {"receiver":"wechat","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"yellow"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 120 seconds.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:28:23.089Z","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"a8c518a872e3149a"}],"groupLabels":{"instance":"10.3.205.79:9100"},"commonLabels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"yellow"},"commonAnnotations":{"description":"10.3.205.79:9100 of job host has been down for more than 120 seconds.","summary":"Instance 10.3.205.79:9100 down"},"externalURL":"http://alertmanager:9093","version":"4","groupKey":"{}/{job=~\"dcagent|host\"}:{instance=\"10.3.205.79:9100\"}","truncatedAlerts":0}
+
+	   2025/05/28 10:28:53 ======= HTTP 请求结束 =======
+
+
+	   2025/05/28 10:32:53 ======= HTTP 请求结束 =======
+	   2025/05/28 10:36:53 ======= HTTP 请求开始 =======
+	   2025/05/28 10:36:53 POST / HTTP/1.1
+	   Host: 192.168.2.17:9094
+	   Content-Length: 951
+	   Content-Type: application/json
+	   User-Agent: Alertmanager/0.24.0
+
+	   {"receiver":"wechat","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"orange"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 10 minutes.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:36:23.089Z","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"6a95ebcc150ee0f4"}],"groupLabels":{"instance":"10.3.205.79:9100"},"commonLabels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"orange"},"commonAnnotations":{"description":"10.3.205.79:9100 of job host has been down for more than 10 minutes.","summary":"Instance 10.3.205.79:9100 down"},"externalURL":"http://alertmanager:9093","version":"4","groupKey":"{}/{job=~\"dcagent|host\"}:{instance=\"10.3.205.79:9100\"}","truncatedAlerts":0}
+
+	   2025/05/28 10:36:53 ======= HTTP 请求结束 =======
+
+
+	   2025/05/28 10:56:53 ======= HTTP 请求开始 =======
+	   2025/05/28 10:56:53 POST / HTTP/1.1
+	   Host: 192.168.2.17:9094
+	   Content-Length: 945
+	   Content-Type: application/json
+	   User-Agent: Alertmanager/0.24.0
+
+	   {"receiver":"wechat","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"red"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 30 minutes.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:56:23.089Z","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"8b581f60e6fd0de1"}],"groupLabels":{"instance":"10.3.205.79:9100"},"commonLabels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"red"},"commonAnnotations":{"description":"10.3.205.79:9100 of job host has been down for more than 30 minutes.","summary":"Instance 10.3.205.79:9100 down"},"externalURL":"http://alertmanager:9093","version":"4","groupKey":"{}/{job=~\"dcagent|host\"}:{instance=\"10.3.205.79:9100\"}","truncatedAlerts":0}
+
+	   2025/05/28 10:56:53 ======= HTTP 请求结束 =======
+	   2025/05/28 11:56:53 ======= HTTP 请求开始 =======
+	   2025/05/28 11:56:53 POST / HTTP/1.1
+	   Host: 192.168.2.17:9094
+	   Content-Length: 945
+	   Content-Type: application/json
+	   User-Agent: Alertmanager/0.24.0
+
+	   {"receiver":"wechat","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"red"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 30 minutes.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:56:23.089Z","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"8b581f60e6fd0de1"}],"groupLabels":{"instance":"10.3.205.79:9100"},"commonLabels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"red"},"commonAnnotations":{"description":"10.3.205.79:9100 of job host has been down for more than 30 minutes.","summary":"Instance 10.3.205.79:9100 down"},"externalURL":"http://alertmanager:9093","version":"4","groupKey":"{}/{job=~\"dcagent|host\"}:{instance=\"10.3.205.79:9100\"}","truncatedAlerts":0}
+
+	   2025/05/28 12:26:53 ======= HTTP 请求开始 =======
+	   2025/05/28 12:26:53 POST / HTTP/1.1
+	   Host: 192.168.2.17:9094
+	   Content-Length: 1762
+	   Content-Type: application/json
+	   User-Agent: Alertmanager/0.24.0
+
+	   {"receiver":"wechat","status":"resolved","alerts":[{"status":"resolved","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"orange"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 10 minutes.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:36:23.089Z","endsAt":"2025-05-28T04:26:23.089Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"6a95ebcc150ee0f4"},{"status":"resolved","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"red"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 30 minutes.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:56:23.089Z","endsAt":"2025-05-28T04:26:23.089Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"8b581f60e6fd0de1"},{"status":"resolved","labels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host","severity":"yellow"},"annotations":{"description":"10.3.205.79:9100 of job host has been down for more than 120 seconds.","summary":"Instance 10.3.205.79:9100 down"},"startsAt":"2025-05-28T02:28:23.089Z","endsAt":"2025-05-28T04:26:23.089Z","generatorURL":"http://prometheus:9090/graph?g0.expr=up+%3D%3D+0\u0026g0.tab=1","fingerprint":"a8c518a872e3149a"}],"groupLabels":{"instance":"10.3.205.79:9100"},"commonLabels":{"alertname":"instance-down","instance":"10.3.205.79:9100","job":"host"},"commonAnnotations":{"summary":"Instance 10.3.205.79:9100 down"},"externalURL":"http://alertmanager:9093","version":"4","groupKey":"{}/{job=~\"dcagent|host\"}:{instance=\"10.3.205.79:9100\"}","truncatedAlerts":0}
+	*/
+
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "读取请求体失败", http.StatusBadRequest)
+		return
+	}
+	r.Body.Close()
+
+	// 解析 JSON 为 template.Data
+	var data template.Data
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		http.Error(w, "解析 JSON 失败："+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// 计算 fingerprint，保持 startsAt
+	for i := range data.Alerts {
+		alert := &data.Alerts[i]
+
+		// 将 Labels 转为 model.LabelSet，但排除 severity
+		ls := model.LabelSet{}
+		for k, v := range alert.Labels {
+			if k == "severity" {
+				continue
+			}
+			ls[model.LabelName(k)] = model.LabelValue(v)
+		}
+
+		// 篡改 Fingerprint
+		fp := ls.Fingerprint()
+		alert.Fingerprint = fp.String()
+
+		if model.AlertStatus(alert.Status) == model.AlertResolved {
+			store.Delete(fp)
+		} else {
+			// 修正 StartsAt
+			alert.StartsAt = store.SetIfAbsent(fp, alert.StartsAt)
+		}
+
+	}
+
+	// 重编码
+	newBody, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "重新编码请求体失败", http.StatusInternalServerError)
+		return
+	}
+
+	proxyReq, err := http.NewRequest(r.Method, proxyURL.String(), bytes.NewBuffer(newBody))
 	if err != nil {
 		http.Error(w, "构造请求失败", http.StatusInternalServerError)
 		return
 	}
-
-	// 复制请求头
 	proxyReq.Header = r.Header.Clone()
+
+	printRequest(proxyReq)
 
 	client := &http.Client{}
 	resp, err := client.Do(proxyReq)
@@ -95,18 +239,14 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	// 设置响应头
 	for k, vv := range resp.Header {
 		for _, v := range vv {
 			w.Header().Add(k, v)
 		}
 	}
-
-	// 写入状态码 + 响应体
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 }
-
 func main() {
 	parseFlags()
 
